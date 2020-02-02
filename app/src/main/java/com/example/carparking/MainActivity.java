8e7b15b7dd04670d13d5b8bff82dd318dea054a8
vova200020 <->
2020-02-02 20:08:37 +0000

    sprint 1
    i have amade a log in screen that would let user to log in and sign up. i have connected my app to Firebase and created options to log in with existing ccount or create a new account and log in.  Also i have added a function that keeps user logged in when the app is closed but i had to comment it as it makes it difficult to test

---
+++ app/src/main/java/com/example/carparking/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.carparking;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothA2dp;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+    EditText emailE;
+    EditText passwordE;
+    String email;
+    String password;
+    private FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         emailE = findViewById(R.id.Email);
+         passwordE = findViewById(R.id.Password);
+         firebaseAuth= FirebaseAuth.getInstance();
+         FirebaseUser user =firebaseAuth.getCurrentUser();
+
+         //goes to second activity if user is logged in
+//         if (user !=null){
+//             finish();
+//             startActivity(new Intent(MainActivity.this,logged.class));
+//         }
+
+        }
+        public void SignUp (View view){
+
+         email= emailE.getText().toString().trim();
+         password = passwordE.getText().toString().trim();
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, " You have been sucesfully signed up", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this,logged.class));
+                }
+                else {
+                    Toast.makeText(MainActivity.this, " Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        }
+        public void LogIn (View view){
+
+          firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+              @Override
+              public void onComplete(@NonNull Task<AuthResult> task) {
+                  if(task.isSuccessful()){
+                      //FirebaseUser user = firebaseAuth.getCurrentUser();
+                      Toast.makeText(MainActivity.this, " You have been sucesfully Loged in", Toast.LENGTH_SHORT).show();
+                      startActivity(new Intent(MainActivity.this,logged.class));
+                  }
+                  else{
+                      Toast.makeText(MainActivity.this, " Failed", Toast.LENGTH_SHORT).show();
+                  }
+              }
+          });
+
+          firebaseAuth.signOut();
+
+
+
+
+
+
+
+        }
+}
+
